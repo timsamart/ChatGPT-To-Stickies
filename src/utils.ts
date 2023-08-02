94094eca42775ed481789a03c366df8886c5bed4
Timotheos Samartzidis <timos@outlook.com>
2023-08-02 14:02:14 +0200

    Added support for just content

--- src/utils.ts
+++ src/utils.ts
@@ -1,26 +1,50 @@
-// Parser function
+// utils.ts
+
 export function parseNotes(
   notes: string
 ): { title: string; content: string }[] {
-  const lines = notes.split("\n");
-  let entries = [];
+  const noteLines = notes.replace(/\*\*/g, "").split("\n");
 
-  lines.forEach((line) => {
-    line = line.trim().replace(/\*\*/g, ""); // Trim spaces and remove **
+  let title = "";
+  let content = "";
 
-    if (!line) {
-      // Skip empty lines
-      return;
+  const entries: { title: string; content: string }[] = [];
+
+  noteLines.forEach((line, index) => {
+    line = line.trim();
+
+    if (line === "") {
+      // If the line is empty, we've found a new paragraph/note
+      if (title !== "" || content.trim() !== "") {
+        // ignore empty content
+        entries.push({ title, content: content.trim() });
+      }
+      title = "";
+      content = "";
+    } else if (line.includes(":")) {
+      if (line.endsWith(":")) {
+        // if line ends with ':' treat it as content
+        content += ` ${line.trim().slice(0, -1)}`; // remove ':' from the end
+      } else {
+        if (title !== "" || content.trim() !== "") {
+          // new note found, push the previous one
+          entries.push({ title, content: content.trim() });
+          content = "";
+        }
+        const lineParts = line.split(":");
+        title = lineParts[0].trim();
+        content += lineParts.slice(1).join(":").trim(); // handles multiple ":" in the line
+      }
+    } else {
+      content += ` ${line.trim()}`;
     }
 
-    if (line.includes(":")) {
-      // Title: Content structure
-      const parts = line.split(":");
-      const entry = {
-        title: parts[0].trim(),
-        content: parts.slice(1).join(":").trim(),
-      };
-      entries.push(entry);
+    // if it is the last line, push the content
+    if (
+      index === noteLines.length - 1 &&
+      (title !== "" || content.trim() !== "")
+    ) {
+      entries.push({ title, content: content.trim() });
     }
   });
 
--- test/code.test.js
+++ test/code.test.js
@@ -1,13 +1,37 @@
+// code.test.js
+
 import { parseNotes } from "../src/utils";
 
 describe("parseNotes", () => {
-  test("should parse notes correctly and remove **", () => {
-    const notes =
-      "**Title1**: Content1\n\n**Title2**: Content2\n**Title3**: Content3";
+  test("should parse notes correctly, handle notes without titles, handle multiple ':' and ignore empty lines", () => {
+    const notes = `
+      **Title1**: Content1
+
+      Content2
+      Content2secondpart
+
+      **Title2**: 
+
+      **Title3**: Content:3:with:colons
+
+      **Title4**
+
+      **Title5**: Content5
+
+      **Title6**: Content6
+      Title7: Content7
+      Title8: Content8
+    `;
     const expected = [
       { title: "Title1", content: "Content1" },
-      { title: "Title2", content: "Content2" },
-      { title: "Title3", content: "Content3" },
+      { title: "", content: "Content2 Content2secondpart" },
+      { title: "", content: "Title2" },
+      { title: "Title3", content: "Content:3:with:colons" },
+      { title: "", content: "Title4" },
+      { title: "Title5", content: "Content5" },
+      { title: "Title6", content: "Content6" },
+      { title: "Title7", content: "Content7" },
+      { title: "Title8", content: "Content8" },
     ];
     expect(parseNotes(notes)).toEqual(expected);
   });
